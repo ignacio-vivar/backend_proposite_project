24148bb3857af8c0542c1af52434540562626831
Ignacio <jjivc10@gmail.com>
2026-01-02 19:09:13 -0300

    fix db problem

--- app/database/database.py
+++ app/database/database.py
@@ -10,6 +10,9 @@ from app.core.config import DATABASE_URL
 engine = create_async_engine(
     DATABASE_URL,
     pool_pre_ping=True,
+    connect_args={
+        "statement_cache_size": 0,
+    },
 )
 
 AsyncSessionLocal = async_sessionmaker(
